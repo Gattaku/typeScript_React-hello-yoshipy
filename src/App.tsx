3a39df1e029f1183fef5963fa0c86cafc45af843
Gattaku <kazuyoshi.ogata.ab@gmail.com>
2023-04-28 16:27:44 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Hello from "./compornents/Hello"
 
 function App() {
+  const text = {
+    a:1,
+    b:2,
+    message: "I am Peter",
+  }
+  const [isAuth, setIsAuth] = useState(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hello text= {text} isAuth={isAuth}></Hello>
     </div>
   );
 }
---
+++ src/compornents/Hello.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+type HelloProps = {
+    text: {
+        a: number;
+        b: number;
+        message: string;
+    }
+    isAuth: boolean;
+}
+
+const Hello = (props: HelloProps) => {
+    // const {message: string} = props.text.message;
+    const {isAuth} = props;
+    const {text} = props;
+  return (
+    <div>
+        <h1>hello {text.message}</h1>
+        <h2>{`値は${props.isAuth}です！`}</h2>
+    </div>
+  )
+}
+
+export default Hello
